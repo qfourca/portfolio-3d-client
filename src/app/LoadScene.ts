63441176e25ef86175a2664e4a9b91844409db1d
kim geon ho <80884472+qfourca@users.noreply.github.com>
2023-06-22 10:13:19 +0900

    [FEAT] new room

--- src/app/LoadScene.ts
+++ src/app/LoadScene.ts
@@ -1,18 +1,17 @@
-import { Mesh, SceneLoader, Vector3 } from '@babylonjs/core';
+import { Mesh, SceneLoader, SpotLight, Vector3 } from '@babylonjs/core';
 import room_model from '$static/room3.glb';
 import GlobalScene from '$/global/scene/Scene';
 
 export default async () => {
   const load = await SceneLoader.AppendAsync('', room_model, GlobalScene._);
-  // (load.rootNodes[load.rootNodes.length - 1] as Mesh).scaling = new Vector3(
-  //   0.01,
-  //   0.01,
-  //   0.01
-  // );
-
   load.lights.forEach((l) => {
-    console.log(l);
-    l.intensity = l.intensity / 100;
+    l.intensity = l.intensity / 200;
   });
+  const mainLight = load
+    .getTransformNodeByName('main')!
+    .getChildren()[0] as SpotLight;
+
+  mainLight.angle = Math.PI;
+  mainLight.innerAngle = Math.PI;
   return load;
 };
