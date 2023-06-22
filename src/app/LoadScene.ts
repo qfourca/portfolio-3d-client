6b3d60ab83ea62d61f1b6085c490cc3029f0a012
kim geon ho <80884472+qfourca@users.noreply.github.com>
2023-06-22 11:00:41 +0900

    [FEAT] shadow

--- src/app/LoadScene.ts
+++ src/app/LoadScene.ts
@@ -1,17 +1,36 @@
-import { Mesh, SceneLoader, SpotLight, Vector3 } from '@babylonjs/core';
+import {
+  Mesh,
+  SceneLoader,
+  ShadowGenerator,
+  SpotLight,
+  Vector3,
+} from '@babylonjs/core';
 import room_model from '$static/room3.glb';
 import GlobalScene from '$/global/scene/Scene';
 
 export default async () => {
   const load = await SceneLoader.AppendAsync('', room_model, GlobalScene._);
   load.lights.forEach((l) => {
-    l.intensity = l.intensity / 200;
+    l.intensity = l.intensity / 10;
   });
   const mainLight = load
     .getTransformNodeByName('main')!
     .getChildren()[0] as SpotLight;
 
-  mainLight.angle = Math.PI;
-  mainLight.innerAngle = Math.PI;
+  mainLight.angle = (210 / 180) * Math.PI;
+  mainLight.innerAngle = (210 / 180) * Math.PI;
+
+  const shadowGenerator = new ShadowGenerator(2048, mainLight);
+  shadowGenerator.useBlurExponentialShadowMap = true;
+  shadowGenerator.blurScale = 4;
+  shadowGenerator.filter = ShadowGenerator.FILTER_PCF;
+  shadowGenerator.bias = 0.0001;
+  //@ts-expect-error
+  load.meshes.forEach((mesh: Mesh) => {
+    mesh.receiveShadows = true;
+    console.log(mesh.name);
+    shadowGenerator.addShadowCaster(mesh, false);
+  });
+  shadowGenerator.removeShadowCaster(load.getMeshByName('평면')!);
   return load;
 };
