2d72427bfc4248005bb20abcad21fb5bea522c58
kim geon ho <80884472+qfourca@users.noreply.github.com>
2023-06-27 12:14:30 +0900

    [FEAT] video in sofa

--- src/Interaction/SofaView.ts
+++ src/Interaction/SofaView.ts
@@ -1,11 +1,36 @@
-import { Mesh, TransformNode, Vector3 } from '@babylonjs/core';
+import {
+  Mesh,
+  PBRMaterial,
+  Texture,
+  TransformNode,
+  Vector3,
+  VideoTexture,
+} from '@babylonjs/core';
 import AbstarctChild from './architecture/AbstarctChild';
 import GlobalScene from '$/global/scene/Scene';
 import Parentable from './architecture/Parentable';
 
+import TestVideo from '$static/video.mp4';
+
 export default class SofaView extends AbstarctChild {
+  private tvMaterial: PBRMaterial;
+
   constructor(private sofa: TransformNode, parent: Parentable<AbstarctChild>) {
     super(sofa, parent);
+    this.tvMaterial = sofa
+      .getChildren()
+      .find(({ name }) => name === 'tv')
+      ?.getChildMeshes()
+      .find(({ name }) => name === 'TV_disp')?.material as PBRMaterial;
+
+    this.tvMaterial.albedoTexture = new VideoTexture(
+      'video',
+      TestVideo,
+      GlobalScene._
+    );
+    const video = (this.tvMaterial.albedoTexture as VideoTexture).video;
+    video.muted = true;
+    video.autoplay = false;
   }
   click(): void {
     const camera = GlobalScene._.camera;
@@ -15,6 +40,10 @@ export default class SofaView extends AbstarctChild {
     const { x, y, z } = this.sofa.getAbsolutePosition();
     camera.smoothMove(new Vector3(x, y + 1.2, z), time);
     camera.smoothRotation(new Vector3(0, Math.PI / 2, 0), time);
+
+    setTimeout(() => {
+      (this.tvMaterial.albedoTexture as VideoTexture).video.play();
+    }, time);
 
     super.click();
   }
